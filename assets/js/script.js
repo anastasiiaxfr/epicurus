815f148e78693137ef3ba24d71631833011675f9
anastasiiaxfr <anastasiiaberest@gmail.com>
2023-04-26 13:47:37 +0300

    popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,56 @@
-//Cookie
+//MENU TOGGLE
+let navbar_toggle = document.querySelector('.navbar-toggler');
+let navbar_toggle_open = document.querySelector('.navbar-toggler-open');
+let navbar_toggle_close = document.querySelector('.navbar-toggler-close');
+
+navbar_toggle.addEventListener('click', function () {
+    navbar_toggle_close.classList.toggle('d-block')
+    navbar_toggle_open.classList.toggle('d-none')
+});
+
+
+//COLLAPSE
+let collapse_toggle = document.querySelector('.toc__header');
+let collapse_body = document.querySelector('.toc__body');
+if (collapse_toggle) {
+    collapse_toggle.addEventListener('click', function () {
+        this.firstElementChild.classList.toggle('active');
+        collapse_body.classList.toggle('d-block');
+    })
+};
+
+
+//TEXT COPY
+function copy(el) {
+    let text_parent = document.querySelector(el);
+    let text = '';
+    document.querySelectorAll(`${el} li`).forEach(li => {
+        text += li.textContent + '\n';
+    });
+    let text_copy = document.createElement('span');
+    text_copy.classList.add('copy');
+    text_copy.textContent = 'Copy all IPs';
+
+    text_parent && text_parent.parentNode.insertBefore(text_copy, text_parent.nextSibling);
+
+    text_copy.addEventListener('click', async () => {
+        text_copy.classList.toggle('active');
+        text_parent.classList.toggle('active');
+        try {
+            await navigator.clipboard.writeText(text);
+            //console.log(text);
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+        }
+
+    });
+}
+
+copy('#p4 + ul');
+copy('#p3 + ul');
+
+
+//COOKIE
 function setCookie(name, value, days) {
     var expires = "";
     if (days) {
@@ -24,7 +76,7 @@ function removeCookie(name) {
     document.cookie = name + "=; Max-Age=-99999999;";
 }
 
-// Switcher
+// SWITCHER
 if (getCookie("theme") === "white") {
     $("body").addClass("white");
     $(".switch input[type=checkbox]").prop("checked", true);
@@ -71,7 +123,7 @@ function getKey() {
 
     fetch('https://api.sendpulse.com/oauth/access_token', requestOptions)
         .then(response => response.json())
-        .then(data => setCookie("access_token", data.access_token, 1/4))
+        .then(data => setCookie("access_token", data.access_token, 1 / 4))
         .catch(error => console.error(error));
 }
 
@@ -84,8 +136,9 @@ const emailData = {
 getKey();
 let access_token = getCookie("access_token");
 
+//ADD_USER_SENDPULSE
 function sendRegForm() {
-    setCookie("form_send", true, 1/4);
+    setCookie("form_send", true, 1 / 4);
 
     fetch(apiUrl, {
         method: "POST",
@@ -98,12 +151,39 @@ function sendRegForm() {
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
+
+            show_form_send()
         })
         .catch((error) => console.error(error));
 }
 
+//LOGIN_SENDPULSE
+let check_login_true;
+function getAuthData(login_email, login_password) {
+    fetch(apiUrl, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
 
+            const subscriber = data.find(subscriber => subscriber.email === login_email && subscriber.variables.password === login_password);
+            if (subscriber) {
+                //alert(true);
+                check_login_true = true;
+                check_login_from(check_login_true);
+            } else {
+                //alert(false);
+                check_login_true = false;
+                check_login_from(check_login_true);
+            }
+        })
+        .catch(error => console.error(error));
+}
 
+//FORMS SEND DATA
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
 const forms = document.querySelectorAll(".needs-validation");
 
@@ -128,13 +208,19 @@ const forms = document.querySelectorAll(".needs-validation");
                     if (emailInput && passwordInput) {
                         const data = {
                             email: emailInput.value,
+
                             variables: {
                                 password: passwordInput.value,
                             },
                         };
+
                         emailData.emails.push(data);
                         //alert(emailData.emails[0].email + emailData.emails[0].variables.password);
                         sendRegForm();
+                        
+                        // show_success_modal();
+                        //setTimeout(window.location.replace("/"), 4000);
+                        //setTimeout(form.reset(), 1500);
                     }
                 }
             },
@@ -144,8 +230,38 @@ const forms = document.querySelectorAll(".needs-validation");
 })();
 
 
+//FORM LOGIN VALIDATE
+const form_login = document.querySelector(".from-validation");
+const form_login_btn = document.querySelector(".from-validation .btn");
 
-// Form
+form_login.addEventListener(
+    "submit",
+    function (event) {
+        this.closest(".from-validation").classList.add("was-validated");
+        if (!this.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            event.preventDefault()
+            event.stopPropagation()
+
+            const emailInput = this.closest(".from-validation").querySelector('input[type="email"]');
+            const passwordInput = this.closest(".from-validation").querySelector('input[type="password"]');
+            if (emailInput && passwordInput) {
+                login_email = emailInput.value;
+                login_password = passwordInput.value;
+                //alert(login_email);
+                //alert(login_password);
+                getAuthData(login_email, login_password);
+                //setTimeout(form_login.reset(), 1500);
+            }
+        }
+    },
+    false
+);
+
+
+// FORM
 const password = document.querySelector("#reg_password");
 const password_login = document.querySelector("#login_password");
 const password_reset = document.querySelector("#reset_password");
@@ -186,25 +302,34 @@ function passwordValidity(e) {
     }
 }
 
-password_confirm.addEventListener("input", function (e) {
-    passwordConfirmValidity(e);
-});
+if (password_confirm) {
+    password_confirm.addEventListener("input", function (e) {
+        passwordConfirmValidity(e);
+    });
+}
+if (password_confirm) {
+    password_confirm.addEventListener("change", function (e) {
+        passwordConfirmValidity(e);
+    });
+}
 
-password_confirm.addEventListener("change", function (e) {
-    passwordConfirmValidity(e);
-});
+if (password) {
+    password.addEventListener("input", function (e) {
+        passwordValidity(e);
+    });
+}
 
-password.addEventListener("input", function (e) {
-    passwordValidity(e);
-});
+if (password_login) {
+    password_login.addEventListener("input", function (e) {
+        passwordValidity(e);
+    });
+}
 
-password_login.addEventListener("input", function (e) {
-    passwordValidity(e);
-});
-
-password_reset.addEventListener("input", function (e) {
-    passwordValidity(e);
-});
+if (password_reset) {
+    password_reset.addEventListener("input", function (e) {
+        passwordValidity(e);
+    });
+}
 
 const checkboxes = document.querySelectorAll('form input[type="checkbox"]');
 checkboxes.forEach(function (checkbox) {
@@ -233,21 +358,110 @@ input.forEach((inputElement) => {
     });
 });
 
+
+//FORM LOGIN CHECKED
+const formLogin = document.querySelector("#modalSignIn");
+const formLogin_error = formLogin.querySelector(".invalid-feedback-login");
+const formLogin_fields = formLogin.querySelectorAll("input");
+const formLoginSuccess = document.querySelector("#modalLoginSuccess");
+const formLoginSuccess_btn = formLoginSuccess.querySelector(".btn");
+
+//const check_login_true = getCookie("login_true");
+function set_login_form() {
+    formLogin.querySelector("input").validity.valid;
+    formLogin_error.classList.remove("d-block");
+    formLogin_fields.forEach(i => {
+        i.setCustomValidity("");
+        i.classList.remove("is-invalid");
+    });
+    window.location.href = "https://www.google.com/";
+};
+
+function reset_login_form() {
+    formLogin_error.classList.add("d-block");
+    formLogin_fields.forEach(i => {
+        i.setCustomValidity("Invalid input");
+        i.classList.add("is-invalid");
+    });
+};
+
+function check_login_from(check_login_true) {
+    if (check_login_true === true) {
+        set_login_form();
+        //alert(check_login_true);
+    } else if (check_login_true === false) {
+        reset_login_form();
+        //alert(check_login_true);
+    }
+};
+
+
+//FORM SEND SUCCESS
 const formSend = document.querySelector("#modalSuccessSend");
 let check_form_send = getCookie("form_send");
-check_form_send === "true" && setTimeout(window.location.replace("/"), 1000);
-if (check_form_send === "true") {
-    formSend.classList.add('d-block', 'show');
+let form_send_close = formSend.querySelector(".modal-close");
+
+function show_form_send(){
+
+};
+
+if (check_form_send) {
+    if (check_form_send === "true") {
+        formSend.classList.add('d-block', 'show');
+
+    } else {
+        formSend.classList.remove('d-block', 'show');
+};
+form_send_close.addEventListener('click', function () {
     removeCookie("form_send");
-    //FIXME need correct reset from
+    window.location.replace("/");
+});
+
+formSend.addEventListener("click", function (e) {
+    removeCookie("form_send");
+    window.location.replace("/");
+});
+};
+
+
+//USER CLICK ON LINK INSIDE EMAIL
+const urlParams = new URLSearchParams(window.location.search);
+const emailParam = urlParams.get('email');
+
+const formEmailClicked = document.querySelector("#modalEmailClicked");
+const formEmailClicked_btn = formEmailClicked.querySelector(".btn");
+const formEmailClicked_close = formEmailClicked.querySelector(".modal-close");
+
+if (emailParam === 'checked') { setCookie("email_confirm", "true", 1); }
+const email_confirm = getCookie("email_confirm");
+
+if (email_confirm === "true") {
+    formEmailClicked.classList.add('d-block', 'show');
+} else {
+    formEmailClicked.classList.remove('d-block', 'show');
+    removeCookie("email_confirm");
 }
+formEmailClicked_btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    removeCookie("email_confirm");
+    window.location.href = "https://www.google.com/";
+});
+formEmailClicked_close.addEventListener('click', function () {
+    removeCookie("email_confirm");
+    window.location.replace("/");
+    urlParams.delete('email');
+    const newUrl = window.location.origin + window.location.pathname + '?' + urlParams.toString();
+    window.history.replaceState(null, null, newUrl);
+});
+formEmailClicked.addEventListener("click", function (event) {
+    if (event.target === formEmailClicked) {
+        removeCookie("email_confirm");
+        //window.location.replace("/"); NOT needed
+    }
+});
 
-const formAll = document.querySelectorAll('form');
-// check_form_send === true && formAll.forEach(i => {
-//     i.reset();
-// });
 
-// MODAL
+// MODAL (FIXME https://getbootstrap.com/docs/5.0/components/modal/#toggle-between-modals)
 const modal = document.querySelectorAll(".modal");
 const modal_toggle = document.querySelectorAll(
     '.modal [data-bs-toggle="modal"]'
@@ -300,3 +514,4 @@ modal_close.forEach(function (ell) {
         }
     });
 });
+
